056a3550a512b6e4878d44354149caf53116fb3e
Соджиро <koyama10032009@outlook.jp>
2022-10-16 12:02:28 +0900

    bounds: change return value into Result to distinguish whether the given value found or not correctly

--- src/bounds.rs
+++ src/bounds.rs
@@ -2,21 +2,30 @@ use std::cmp::Ordering;
 
 // Returns an iterator to specified bound that pointing to the first element in the range.
 pub trait Bound<T> {
-    fn lower_bound(&self, x: T) -> usize;
-    fn upper_bound(&self, x: T) -> usize;
+    fn lower_bound(&self, x: T) -> Result<usize, usize>;
+    fn upper_bound(&self, x: T) -> Result<usize, usize>;
 }
 
 impl<T: Ord> Bound<T> for [T] {
     /// Returns an iterator pointing to the first element in the range [first,last) which does not compare less than val.
+    /// If the iterator is empty, the function returns Err(0).
+    /// If the given element larger than all elements in the range, the function returns Err(length of the range).
     /// ```
     /// use k0i::bounds::Bound;
     /// let vec = vec![1, 2, 4, 6];
-    /// assert_eq!(vec.lower_bound(3), 2);
-    /// assert_eq!(vec.lower_bound(0), 0);
-    /// assert_eq!(vec.lower_bound(7), 4);
-    /// assert_eq!(vec.lower_bound(2), 1);
+    /// assert_eq!(vec.lower_bound(2), Ok(1));
+    /// assert_eq!(vec.lower_bound(3), Ok(2));
+    /// assert_eq!(vec.lower_bound(0), Ok(0));
+    /// assert_eq!(vec.lower_bound(6), Ok(3));
+    /// assert_eq!(vec.lower_bound(7), Err(4));
     /// ```
-    fn lower_bound(&self, x: T) -> usize {
+    fn lower_bound(&self, x: T) -> Result<usize, usize> {
+        if self.is_empty() {
+            return Err(0);
+        }
+        if self.iter().last().unwrap() < &x {
+            return Err(self.len());
+        }
         let mut low = 0;
         let mut high = self.len();
 
@@ -31,19 +40,27 @@ impl<T: Ord> Bound<T> for [T] {
                 }
             }
         }
-        low
+        Ok(low)
     }
     /// Returns an iterator pointing to the first element in the range [first,last) which compares greater than val.
+    /// If the iterator is empty, the function returns Err(0).
+    /// If the given element larger than all elements in the range, the function returns Err(length of the range).
     /// ```
     /// use k0i::bounds::Bound;
     /// let vec = vec![1, 2, 4, 6];
-    /// assert_eq!(vec.upper_bound(4), 3);
-    /// assert_eq!(vec.upper_bound(0), 0);
-    /// assert_eq!(vec.upper_bound(3), 2);
-    /// assert_eq!(vec.upper_bound(6), 4);
-    /// assert_eq!(vec.upper_bound(7), 4);
+    /// assert_eq!(vec.upper_bound(4), Ok(3));
+    /// assert_eq!(vec.upper_bound(0), Ok(0));
+    /// assert_eq!(vec.upper_bound(3), Ok(2));
+    /// assert_eq!(vec.upper_bound(6), Err(4));
+    /// assert_eq!(vec.upper_bound(7), Err(4));
     /// ```
-    fn upper_bound(&self, x: T) -> usize {
+    fn upper_bound(&self, x: T) -> Result<usize, usize> {
+        if self.is_empty() {
+            return Err(0);
+        }
+        if self.iter().last().unwrap() <= &x {
+            return Err(self.len());
+        }
         let mut low = 0;
         let mut high = self.len();
 
@@ -58,6 +75,6 @@ impl<T: Ord> Bound<T> for [T] {
                 }
             }
         }
-        low
+        Ok(low)
     }
 }
